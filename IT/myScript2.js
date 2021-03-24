2d278d927c06aa33cbce6cd58b511e7dcf6493c3
InsaneSuperSand <necronomicon93@gmail.com>
2021-03-24 21:42:43 +0100

    Add new section

--- IT/myScript2.js
+++ IT/myScript2.js
@@ -3,10 +3,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "10px 10px";
+    document.getElementById("navbar").style.padding = "5px 4px";
     document.getElementById("logo").style.fontSize = "25px";
   } else {
-    document.getElementById("navbar").style.padding = "20px 10px";
+    document.getElementById("navbar").style.padding = "10px 5px";
     document.getElementById("logo").style.fontSize = "35px";
   }
 }
