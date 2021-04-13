633caeb5b3e5f904777ca3f3982b7d8a22c6a547
InsaneSuperSand <necronomicon93@gmail.com>
2021-04-13 20:26:05 +0200

    add table and effect

--- IT/Cennik/myScript3.js
+++ IT/Cennik/myScript3.js
@@ -18,3 +18,13 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   } 
+
+  $('table tr').each(function(){
+    $(this).find('th').first().addClass('first');
+    $(this).find('th').last().addClass('last');
+    $(this).find('td').first().addClass('first');
+    $(this).find('td').last().addClass('last');
+  });
+  
+  $('table tr').first().addClass('row-first');
+  $('table tr').last().addClass('row-last');
